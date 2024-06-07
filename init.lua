1574d8fba9891ec97e7442459b15199f6c2130c7
Gaurav B <grbagwe@mtu.edu>
2024-06-06 20:53:55 -0400

    remaping the begining and end of the line

--- init.lua
+++ init.lua
@@ -162,6 +162,11 @@ vim.opt.scrolloff = 10
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
+-- Set bb as the begining of the line
+vim.keymap.set('n', '<C-i>', '0')
+vim.keymap.set('n', '<C-a>', '$')
+--
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
